03239f4ad7c463215615392514a440e68e1852e9
hien4621 <hien4621@gmail.com>
2023-08-29 16:57:30 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'antd/dist/reset.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './reduxStore/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ 
+    <Provider store={store}>
+      <Router>
+        <App />
+        <ToastContainer/>
+      </Router>
+    </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
